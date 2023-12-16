57c564e5d770351ff703bd3d0bcfbc08bf319384
meddid <nastiapoletaevaa@gmail.com>
2023-12-16 21:12:36 +0500

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace huy
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static nastybabeEntities1 DB = new nastybabeEntities1();
+    }
+}
